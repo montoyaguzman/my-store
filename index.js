fd0fc0a9c5709454ba7a6ffea13ffe01f2084aa7
montoyaguzman <montoyaguzman7@gmail.com>
2021-10-25 23:40:31 -0500

    Query y request params

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { application } = require('express');
 const express = require('express');
 const app = express();
 const port = 3000;
@@ -11,8 +12,27 @@ app.get('/ruta', (req, res) => {
 });
 
 app.get('/user', (req, res) => {
-    res.json({ id: 1, name: 'jose montoya' });
+    res.json({ name: 'jose montoya', age: 28 });
 });
+
+app.get('/user/:id', (req, res) => {
+    const { id } = req.params;
+    res.json({ id, name: 'jose montoya', age: 28 });
+});
+
+app.get('/user/:id/colors/:red', (req, res) => {
+    const { id, red } = req.params;
+    res.json([ { name: 'rojo' }, { name: 'red' } ]);
+});
+
+app.get('/users', (req, res) => {
+    const { page, size } = req.query;
+    if(page) {
+        res.json({page, size})
+    } else {
+        res.send('parametros obligatorios no informados')
+    }
+})
 
 app.listen(port, () => {
     console.log('ok en puerto: ', port)
