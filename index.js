261967d4baa78c8a6effd0cc0a30856e8766ae00
montoyaguzman <montoyaguzman7@gmail.com>
2021-10-25 23:09:49 -0500

    FEAT : Primer servidor

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log('')
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('hola mundo!!');
+});
+
+app.get('/ruta', (req, res) => {
+    res.send('hola desde otra ruta!!');
+});
+
+app.get('/user', (req, res) => {
+    res.json({ id: 1, name: 'jose montoya' });
+});
+
+app.listen(port, () => {
+    console.log('ok en puerto: ', port)
+});
