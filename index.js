999c52264d26fbd21969b8ab08f6ea5dddc2e835
montoyaguzman <montoyaguzman7@gmail.com>
2021-10-26 12:02:47 -0500

    FEAT : Integra faker

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const { application } = require('express');
 const express = require('express');
+const faker = require('faker');
+
 const app = express();
 const port = 3000;
 
@@ -33,6 +35,28 @@ app.get('/users', (req, res) => {
         res.send('parametros obligatorios no informados')
     }
 })
+
+app.get('/items', (req, res) => {
+    const size = req.query.size || 10;
+    const items = [];
+    for(let i = 0; i <= size; i++) {
+        const item = {
+            name: faker.commerce.productName(),
+            price: faker.commerce.price()
+        }
+        items.push(item)
+    }
+    res.json(items);
+});
+
+app.get('/items/filter', (req, res) => {
+    res.send('yo soy /filter')
+});
+
+app.get('/items/:id', (req, res) => {
+    const { id } = req.params;
+    res.send(`enviaste: ${id}`)
+});
 
 app.listen(port, () => {
     console.log('ok en puerto: ', port)
